735d4c21272640588e22259d1776428ed3a2c35f
Sandeep K A <kasandeep42@gmail.com>
2026-06-21 23:11:44 +0530

    feat: wire auth routes to register and login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import LandingLayout from "@/layouts/LandingLayout";
 import Landing from "./pages/landing";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Route element={<LandingLayout />}>
         <Route path="/" element={<Landing />} />
       </Route>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
